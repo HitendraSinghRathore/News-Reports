1e498949bc21dc516d02e29c0d03416d452e478a
Hitendra <hitendersingh2@gmail.com>
2019-08-09 16:29:01 +0530

    add the first news

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,60 @@
 <template>
-  <v-card
+ <v-container grid-list-md text-center>
+    <v-layout wrap>
+         <v-flex v-for="article in articles" :key="article.title" xs6>
+         <v-card
     class="mx-auto"
-    max-width="60vw"
+    max-width="400"
   >
     <v-img
       class="white--text"
-      height="50vh"
-      src="../assets/default.png"
+      height="200px"
+      v-bind:src="article.urlToImage"
     >
-       </v-img>
+      <v-card-title class="align-end fill-height ">{{article.title}}</v-card-title>
+    </v-img>
 
     <v-card-text>
-    <span class="text-primary text-center">Please select a seach source to see the headlines</span>
-        
+      <span class="align-left">{{article.source.name}}</span><br>
+      <p class="text-primary">{{article.description}}</p>
       
     </v-card-text>
 
-    
+    <v-card-actions>
+      
+      <v-btn
+        text
+        color="white"
+      >
+        Explore More
+      </v-btn>
+    </v-card-actions>
   </v-card>
+      </v-flex>
+    </v-layout>
+ </v-container>
 </template>
 
 <script>
 
-
+import axios from 'axios';
+import key from '../file.js';
 export default {
+    data:()=>({
+        articles:[],
+        error:null
+    })
+    ,
+    created (){
+        axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey='+key.api_key)
+        .then((response)=>{
+            this.articles = response.data.articles;
+            console.log(response.data);
+        })
+        .catch(error =>{
+            this.error = error
+        })
+    }
   
 };
 </script>
