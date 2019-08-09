afae164c9e4e903288c076f2c4762806b46068f1
Hitendra <hitendersingh2@gmail.com>
2019-08-09 13:53:42 +0530

    Added the layout changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
-  <HelloWorld />
+ <div>
+
+
+ </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+
 
 export default {
-  components: {
-    HelloWorld,
-  },
+  
 };
 </script>
